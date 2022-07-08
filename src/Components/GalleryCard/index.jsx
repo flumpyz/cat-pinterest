eb25322a937249a768eeccc50a43d358edb202ca
flumpyz <natalianata2001@mail.ru>
2022-07-08 13:27:36 +0500

    added like button and hover effect for it

--- src/Components/GalleryCard/index.jsx
+++ src/Components/GalleryCard/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as SC from './styles';
 import * as WC from '../../UIComponents/Wrapper/wrappers';
+import LikeButton from "../../UIComponents/LikeButton";
+import {GalleryCardLikeButton} from "./styles";
 
 const Index = (props) => {
     return (
         <SC.GalleryCard>
             <SC.GalleryCardImage src={props.src}/>
+            <SC.GalleryCardLikeButton />
         </SC.GalleryCard>
     );
 };
